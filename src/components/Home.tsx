9d15026ed449e5ed383a78767a9f78c1aa12aa66
arunkumar narayanaswamy <aarcharconsulting@gmail.com>
2024-02-25 22:06:47 +0000

    folder structure

---
+++ src/components/Home.tsx
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <div className="font-funky text-center p-8">
+      <h1 className="text-4xl md:text-6xl text-cyan-600">Hello, World!</h1>
+      <p className="text-lg md:text-xl text-gray-800 mt-4">
+        Welcome to my digital playground! 🎉
+      </p>
+      <p className="text-md md:text-lg text-gray-700 mt-4">
+        I'm a seasoned full-stack developer with a treasure chest of
+        experience—15+ years with .NET and over the past few years im making
+        (Azure, AWS & GCP) my second home. Here, you'll find a few of
+        projects that not only showcase my technical wizardry but also my
+        journey from mere mortals' codes to the cloud nine of software
+        development. 🚀 <br />
+        Strap in and enjoy the ride through my world of code, creativity, and
+        cloud mastery!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    extend: {
+      
+      colors: {
+        orange: '#FF825C', 
+        blue: '#2A2100', 
+      },
+    },
+  },
+
+}
