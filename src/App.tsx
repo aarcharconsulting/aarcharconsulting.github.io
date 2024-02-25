781870a09c0a0912a49a56a87beadc264d3464eb
arunkumar narayanaswamy <aarcharconsulting@gmail.com>
2024-02-25 22:18:47 +0000

    added readme and fixing errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import Home from "./components/Home";
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,16 +5,33 @@ const Home = () => {
       <p className="text-lg md:text-xl text-gray-800 mt-4">
         Welcome to my digital playground! 🎉
       </p>
-      <p className="text-md md:text-lg text-gray-700 mt-4">
-        I'm a seasoned full-stack developer with a treasure chest of
-        experience—15+ years with .NET and over the past few years im making
-        (Azure, AWS & GCP) my second home. Here, you'll find a few of
-        projects that not only showcase my technical wizardry but also my
-        journey from mere mortals' codes to the cloud nine of software
-        development. 🚀 <br />
-        Strap in and enjoy the ride through my world of code, creativity, and
-        cloud mastery!
-      </p>
+
+      <div>
+        <p className="text-md md:text-lg text-gray-700 mt-4">
+          Hello there! 👋 Welcome to my portfolio landing page site. I'm
+          Arunkumar Narayanaswamy. I've built this portfolio to showcase the
+          projects that I'm most proud of.
+        </p>
+        <p className="text-md md:text-lg text-gray-700 mt-4">
+          What's Inside? 🧐 Dive into my portfolio:
+        </p>
+        <p className="text-md md:text-lg text-gray-700 mt-4">
+          A curated collection of my work, demonstrating my problem-solving
+          skills and technical prowess across various domains.
+        </p>
+        <p className="text-md md:text-lg text-gray-700 mt-4">
+          From the early days of .NET to mastering cloud architectures with
+          Azure and AWS, learn about the path I've taken in the tech world.
+        </p>
+        <p className="text-md md:text-lg text-gray-700 mt-4">
+          🌐I'm always on the lookout for new challenges and opportunities.
+          Let's make something amazing together. Drop me an email aarcharconsulting@gmail.com 
+        </p>
+        <p className="text-md md:text-lg text-gray-700 mt-4">
+          Thank you for stopping by, and I hope you enjoy exploring my portfolio
+          as much as I enjoyed building it.🌟
+        </p>
+      </div>
     </div>
   );
 };
